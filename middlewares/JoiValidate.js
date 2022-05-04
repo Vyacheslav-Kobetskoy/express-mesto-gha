0a9087a6adaa0b26e714767c82507859d4fcd8fc
Vyacheslav Kobetskoy <slava.19.07@yandex.ru>
2022-05-04 18:27:09 +0300

    refactor: joi link

--- middlewares/JoiValidate.js
+++ middlewares/JoiValidate.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 
-const avatarJoi = (value, helper) => {
+const linkJoi = (value, helper) => {
   const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/;
 
   if (!regex.test(value)) {
@@ -17,7 +17,7 @@ const patchUserInfoJoi = celebrate({
 
 const patchAvatarJoi = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().uri().custom(avatarJoi),
+    avatar: Joi.string().uri().custom(linkJoi),
   }),
 });
 
@@ -25,7 +25,7 @@ const createUserJoi = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri().custom(avatarJoi),
+    avatar: Joi.string().uri().custom(linkJoi),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
@@ -41,7 +41,7 @@ const loginJoi = celebrate({
 const createCardJoi = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
+    link: Joi.string().required().custom(linkJoi),
   }),
 });
 
