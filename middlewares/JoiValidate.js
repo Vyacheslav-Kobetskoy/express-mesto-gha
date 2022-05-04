a7fb745c15c2260d068c64b5ef7667a157937c79
Vyacheslav Kobetskoy <slava.19.07@yandex.ru>
2022-05-04 11:14:23 +0300

    refactor: Joi card

--- middlewares/JoiValidate.js
+++ middlewares/JoiValidate.js
@@ -40,6 +40,12 @@ const loginJoi = celebrate({
   }),
 });
 
+const createCardJoi = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+});
 module.exports = {
-  getUserIdJoi, patchUserInfoJoi, patchAvatarJoi, createUserJoi, loginJoi,
+  getUserIdJoi, patchUserInfoJoi, patchAvatarJoi, createUserJoi, loginJoi, createCardJoi,
 };
--- routes/cards.js
+++ routes/cards.js
@@ -7,9 +7,10 @@ const {
   deleteCard,
 } = require('../controllers/cards');
 const { auth } = require('../middlewares/auth');
+const { createCardJoi } = require('../middlewares/JoiValidate');
 
 router.get('/cards', auth, getCard);
-router.post('/cards', auth, createCard);
+router.post('/cards', createCardJoi, auth, createCard);
 router.delete('/cards/:cardId', auth, deleteCard);
 router.put('/cards/:cardId/likes', auth, likeCard);
 router.delete('/cards/:cardId/likes', auth, dislikeCard);
