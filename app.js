b926c4bc9c58ea64aa727f54d039f0bd359fae15
Vyacheslav Kobetskoy <slava.19.07@yandex.ru>
2022-05-05 11:03:29 +0300

    refactor: celebrate errors

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 
-const { createUserJoi, loginJoi } = require('./middlewares/JoiValidate');
-const { login, createUser } = require('./controllers/users');
 const UserRouter = require('./routes/users');
 const CardRouter = require('./routes/cards');
-const { errorHandler } = require('./middlewares/errorHandler');
+const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
+const { createUserJoi, loginJoi } = require('./middlewares/JoiValidate');
+const { errorHandler } = require('./middlewares/errorHandler');
 const NotFoundError = require('./error/NotFoundError');
 
 const { PORT = 3000 } = process.env;
@@ -22,13 +23,9 @@ app.post('/signup', createUserJoi, createUser);
 
 app.use(UserRouter);
 app.use(CardRouter);
-app.use(auth, (req, res, next) => next(new NotFoundError('404 Not Found')));
-app.use(errorHandler);
 
-// app.listen(PORT, () => {
-//   // Если всё работает, консоль покажет, какой порт приложение слушает
-//   // eslint-disable-next-line no-console
-//   console.log(`App listening on port ${PORT}`);
-// });
+app.use(auth, (req, res, next) => next(new NotFoundError('404 Not Found')));
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT);
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,6 @@
 module.exports.errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message = 'Ошибка по умолчанию.' } = err;
-  if (message === 'Validation failed') {
-    return res.status(400).send({
-      message: 'Ошибка валидации',
-    });
-  }
-  if (statusCode) {
-    return res.status(statusCode).send({ message });
-  }
-  return next();
+
+  res.status(statusCode).send({ message });
+  next();
 };
